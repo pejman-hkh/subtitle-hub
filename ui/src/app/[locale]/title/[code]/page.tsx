c46d2a244020feadb8b4f5ed37be91d4da456145
pejman <pejman.hkh@gmail.com>
2025-03-11 00:37:48 +0330

    Search with title

---
+++ ui/src/app/[locale]/title/[code]/page.tsx
@@ -0,0 +1,34 @@
+import api from "@/api/api"
+import { currentUrl } from "@/utils/utils"
+import { Metadata } from "next"
+import { getTranslations } from "next-intl/server"
+import { ClientMovie } from "../../movie/[link]/client"
+
+export async function generateMetadata({ params }: {
+    params: Promise<{ code: string }>
+}): Promise<Metadata> {
+    const t = await getTranslations()
+    
+    const { code } = await params
+    const data = await api('/movies/detail?imdb=' + encodeURIComponent(code))
+    const movie = data?.data?.movie
+
+    return {
+        title: `${t("Download Subtitle")} ${movie?.name} ${movie?.year}`,
+        description: `${t("Download Free Subtitle")} ${movie?.name} ${movie?.year}`,
+        keywords: `${t("Download Subtitle") + "," + t("Download Free Subtitle")}`
+    }
+}
+
+export default async function Movie({ params }: {
+    params: Promise<{ code: string }>
+}) {
+
+    const { code } = await params
+    const data = await api('/movies/detail?imdb=' + encodeURIComponent(code))
+    const movie = data?.data?.movie
+
+    return <>
+        <ClientMovie movie={movie} />
+    </>
+}
